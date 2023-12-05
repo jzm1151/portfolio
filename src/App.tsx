06b4266a412be43b082863dc8adc5793a973ff78
jzm1151 <jakemoore271@outlook.com>
2023-12-05 03:25:14 -0500

    update navigation

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,14 @@ function App() {
     <>
       <div className="relative">
         <div className='bg-gray-blue-400 w-full h-20 absolute'></div>
-        <div className='w-full h-20 flex flex-col justify-center p-10 z-100 relative'>
+        <div className='w-full h-20 flex items-center justify-between p-10 z-100 relative'>
           <div className='text-gray-blue-900 text-4xl font-bold'>Jacob Moore</div>
-          <div></div>
+          <div>
+            <a className='ml-2 mr-2 border-2 border-gray-blue-400 transition-all duration-500 hover:pb-2 hover:border-b-gray-blue-700' href='#about'>About</a>
+            <a className='ml-2 mr-2 border-2 border-gray-blue-400 transition-all duration-500 hover:pb-2 hover:border-b-gray-blue-700' href='#projects'>Projects</a>
+            <a className='ml-2 mr-2 border-2 border-gray-blue-400 transition-all duration-500 hover:pb-2 hover:border-b-gray-blue-700' href='#contact'>Contact</a>
+            <a className='ml-2 mr-2 p-2 rounded border-solid border-gray-blue-900 border-2 hover:bg-gray-blue-700 hover:text-gray-blue-100 transition-colors duration-500' href='#resume'>Resume</a>
+          </div>
           <div></div>
         </div>
       </div>
