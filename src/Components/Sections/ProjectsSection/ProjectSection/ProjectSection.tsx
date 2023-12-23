48eb2fd476c17b59b96063c64794e12acd891714
jzm1151 <jakemoore271@outlook.com>
2023-12-22 23:21:24 -0500

    testing to see if the remove some of the complexity from the faces fixes the issue" "

--- src/Components/Sections/ProjectsSection/ProjectSection/ProjectSection.tsx
+++ src/Components/Sections/ProjectsSection/ProjectSection/ProjectSection.tsx
@@ -11,14 +11,15 @@ export interface ProjectSectionProps {
 function ProjectSection(props: {project:ProjectSectionProps, index:number}) {
     return (
         <div className={'cube-face-image image-' + props.index + ' h-full w-full'}>
-            <div className='h-full w-full relative'>
-                <img className="object-cover h-full w-full absolute top-0" src={props.project.img} alt={props.project.alt} />
+            <div className='h-full w-full relative bg-gray-blue-500'>
+                {/* <img className="object-cover h-full w-full absolute top-0" src={props.project.img} alt={props.project.alt} />
                 <div className={'absolute top-0 w-full h-full z-5 flex justify-center items-center gap-2 text-gray-blue-950 font-bold text-xl'}>
                     <p className='text-shadow shadow-gray-blue-400'>View on GitHub</p>
                     <div>
                         <FaArrowDown />
                     </div>
-                </div>
+                </div> */}
+                testing
                 {/* <div className='absolute bottom-0 w-full h-1/5 z-10 flex justify-center items-center text-gray-blue-950 font-bold text-2xl backdrop-blur-2xl'>
                     <a className='flex justify-center items-center gap-2' href={props.project.githubHref} target='_blank'>
                         <div>{props.project.title}</div>
