a374387d8b8c36ddfb4ca8b41c9b9c54faf933b5
jzm1151 <jakemoore271@outlook.com>
2024-01-03 03:03:10 -0500

    add missing resume link

--- src/Components/Sections/GreetingSection.tsx
+++ src/Components/Sections/GreetingSection.tsx
@@ -19,7 +19,7 @@ function GreetingSection() {
                 <h2 className='text-3xl font-bold text-gray-blue-700 mb-3 md:mb-5 md:text-4xl'>{greeting}</h2>
                 <p className='text-4xl font-bold text-gray-blue-800 mb-3 md:mb-5 md:text-5xl'>My name is Jacob Moore</p>
                 <p className='text-xl text-gray-blue-950 mb-7 md:mb-10 md:text-2xl'>I am a full-stack developer specializing in Laravel and Alpine.js</p>
-                <MdBorderBtn link='#resume' text='Resume' blankTarget={true} />
+                <MdBorderBtn link='https://1drv.ms/w/s!AsBprLAmKhHYkVxUv4jgMH8kx7K9?e=5Pr76y' text='Resume' blankTarget={true} />
             </section>
         </section>
     )
