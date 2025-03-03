925a1b80ca867fe982e5f995c56a167fc44906f0
jzm1151 <jakemoore271@outlook.com>
2025-03-02 20:58:27 -0500

    update greeting section

--- src/Components/Sections/GreetingSection.tsx
+++ src/Components/Sections/GreetingSection.tsx
@@ -60,7 +60,25 @@ function GreetingSection() {
                         </svg>
                     </a>{' '}
                     platform. In this role, I focus primarily on building and
-                    maintaining event-driven data pipelines.
+                    maintaining event-driven data pipelines with{' '}
+                    <a
+                        href='https://www.webfx.com/technology/pubsub/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className='inline-flex items-center gap-1 underline'
+                    >
+                        Pub/Sub
+                        <svg
+                            className='fill-gray-blue-950'
+                            xmlns='http://www.w3.org/2000/svg'
+                            height='24px'
+                            viewBox='0 -960 960 960'
+                            width='24px'
+                        >
+                            <path d='M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z' />
+                        </svg>
+                    </a>{' '}
+                    and other technologies.
                 </p>
                 <MdBorderBtn
                     link='https://1drv.ms/w/s!AsBprLAmKhHYkVxUv4jgMH8kx7K9?e=5Pr76y'
